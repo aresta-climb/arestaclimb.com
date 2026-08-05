05f34041a7c027e47a5836767196f720af3ddc49
Renato Utsch <renatoutsch@gmail.com>
2026-08-05 20:41:46 -0300

    Adicionar politica de privacidade editor na config vite.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         cla: resolve(__dirname, 'cla.html'),
         termosDeUso: resolve(__dirname, 'termos-de-uso.html'),
         politicaDePrivacidade: resolve(__dirname, 'politica-de-privacidade.html'),
+        politicaDePrivacidadeEditor: resolve(__dirname, 'politica-de-privacidade-editor.html'),
         privacidadeContribuidores: resolve(__dirname, 'privacidade-contribuidores.html'),
         contato: resolve(__dirname, 'contato.html'),
         editor: resolve(__dirname, 'editor.html')
